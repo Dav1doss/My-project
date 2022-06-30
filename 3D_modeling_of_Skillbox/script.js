e89a4c2159b8dfa498471deeed50fffddf96332b
David Blykha <blykhadavid@gmail.com>
2022-06-30 20:38:03 +0300

    3D modeling of Skillbox

---
+++ 3D_modeling_of_Skillbox/script.js
@@ -0,0 +1,6 @@
+document.querySelector(".header__btn-open").addEventListener("click", function() {
+  document.querySelector(".header__nav").classList.add("active");
+})
+document.querySelector(".nav__close").addEventListener("click", function() {
+  document.querySelector(".header__nav").classList.remove("active");
+})
