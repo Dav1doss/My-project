a46e4fe0657e0cd0bf109ec380e8b224d086e887
Dav1doss <108484208+Dav1doss@users.noreply.github.com>
2022-07-04 19:38:23 +0300

    Delete 3D_modeling_of_Skillbox directory

--- 3D_modeling_of_Skillbox/script.js
+++
@@ -1,6 +0,0 @@
-document.querySelector(".header__btn-open").addEventListener("click", function() {
-  document.querySelector(".header__nav").classList.add("active");
-})
-document.querySelector(".nav__close").addEventListener("click", function() {
-  document.querySelector(".header__nav").classList.remove("active");
-})
